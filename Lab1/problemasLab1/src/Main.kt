61ea4072d6de90ad5bbbab4cab7f518bf1dcc165
CarEsteban <car23016@uvg.edu.gt>
2024-07-18 12:14:16 -0600

    palabras palindromas

--- Lab1/problemasLab1/src/Main.kt
+++ Lab1/problemasLab1/src/Main.kt
@@ -43,6 +43,55 @@ fun problema1() {
 
 fun problema2() {
     println("Problema 2")
+    println("Programa de las palabras palíndromas")
+    var continuar2:Boolean = true
+    val arrayPalabras:MutableList<String> = mutableListOf()
+    var contadorPalabrasPalindromas:Int = 0
+    var contadorPalabrasVocales:Int = 0
+    var contadorPalabrasConsonante:Int = 0
+
+    while (continuar2){
+        println("Ingrese la palabra:")
+        var palabra = readLine()
+        if (palabra != null) {
+            arrayPalabras.add(palabra)
+        }
+        println("Desea ingresar otra palabra? 1)Si 2)No")
+        var opcion = readLine()
+        var opcionInt = opcion?.toInt()
+        if (opcionInt == null){
+            println("Ingrese un número válido")
+        }else if(opcionInt == 1){
+            continuar2 = true
+        }else if(opcionInt == 2){
+            continuar2 = false
+        }
+    }
+    //bucle para contar las palabras palíndromas
+    for (palabra in arrayPalabras){
+        val palabraLimpia = palabra.trim().replace("\\s".toRegex(), "").lowercase()
+        if (palabraLimpia.equals(palabraLimpia.reversed())){
+            contadorPalabrasPalindromas++
+        }
+    }
+
+    //bucle para contar las palabras que contienen al menos 2 vocales distintas
+    val arrayVocales = setOf("a","e","i","o","u")
+    for (palabra in arrayPalabras){
+        for (char in palabra) {
+            
+        }
+    }
+
+
+    //bucle para contar las palabras que inician con una letra consonante
+
+
+    println("Array de palabras ingresado: $arrayPalabras")
+    println("Cantidad de palabras palíndromas del array: $contadorPalabrasPalindromas")
+    println("Cantidad de palabras que contiene al menos 2 vocales distintas: $contadorPalabrasVocales")
+    println("Cantidad de palabras que inician con una letra consonante: $contadorPalabrasConsonante")
+
 }
 
 fun repetirMenu(): Boolean {
