3778f70d0cdca91c9b4ed1ad508985ffc65b5684
CarEsteban <car23016@uvg.edu.gt>
2024-07-17 22:04:33 -0600

    problema1 iniciado

--- Lab1/problemasLab1/src/Main.kt
+++ Lab1/problemasLab1/src/Main.kt
@@ -1,3 +1,34 @@
 fun main() {
-    println("Hello World!")
+    println("1) Problema 1\n2) Problema 2")
+    val problema = readLine()
+    val problemaValor = problema?.toIntOrNull()
+    when (problemaValor) {
+        1 -> problema1()
+        2 -> problema2()
+    }
+}
+
+
+fun problema1() {
+    println("Problema 1")
+    println("Ingrese el valor a convertir de decimal a binario:")
+    val valor = readLine()
+    val valorInt = valor?.toIntOrNull()
+
+    if (valorInt == null) {
+        println("Ingrese un valor válido")
+        return
+    }
+
+    while (valorInt >0){
+        var modulo = valorInt%2
+
+        println(modulo)
+        valorInt/2
+    }
+
+
+}
+fun problema2() {
+    println("Problema 2")
 }
