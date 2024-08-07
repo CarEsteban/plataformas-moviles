79dd66cbc71c8c06856bbd94b07caf6149df2cdf
CarEsteban <car23016@uvg.edu.gt>
2024-08-06 22:15:09 -0600

    implementacion lab 2

---
+++ Lab3/app/src/main/java/com/example/calculator/OperarPostfix.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+class OperarPostfix(private val operacion: String) {
+    private val caracteres = operacion.trim().split(" ")
+    private val stack = Stack<Int>()
+
+    fun mostrarResultado(): String {
+        for (char in caracteres) {
+            if (char.toIntOrNull() != null) {
+                stack.push(char.toInt())
+            } else {
+                val operandoB = stack.pop() // Notar el cambio del orden de los operandos
+                val operandoA = stack.pop()
+
+                when (char) {
+                    "+" -> stack.push(sumar(operandoA, operandoB))
+                    "-" -> stack.push(restar(operandoA, operandoB))
+                    "*" -> stack.push(multiplicar(operandoA, operandoB))
+                    "/" -> stack.push(dividir(operandoA, operandoB))
+                    "^" -> stack.push(potencia(operandoA, operandoB))
+                }
+            }
+        }
+        return stack.peek().toString()
+    }
+
+    private fun sumar(a: Int, b: Int): Int = a + b
+    private fun restar(a: Int, b: Int): Int = a - b
+    private fun multiplicar(a: Int, b: Int): Int = a * b
+    private fun dividir(a: Int, b: Int): Int = a / b
+    private fun potencia(a: Int, b: Int): Int = Math.pow(a.toDouble(), b.toDouble()).toInt()
+}
+
