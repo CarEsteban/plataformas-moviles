5e165904cae2f6a3398620f22b3b89f09e505554
CarEsteban <car23016@uvg.edu.gt>
2024-08-24 17:30:20 -0600

    cambios del diseño UI

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.lab4
 
+
 import android.os.Bundle
+import androidx.compose.ui.graphics.Color
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,105 +47,160 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Calculadora(modifier: Modifier = Modifier){
+fun Calculadora(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+){
 
     var operacion by remember { mutableStateOf("") }
     var resultado by remember { mutableStateOf("0") }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End
+    ) {
+
         DisplayCalculadora(resultado, operacion)
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "X^y")
+        Column(
+            modifier = Modifier
+                .height(510.dp)
+                .padding(bottom = 20.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                BotonCustom(text = "X^y", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "√X", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "(", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = ")", onClick = { /*TODO*/ },modifier = Modifier)
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "√X")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                BotonCustom(text = "AC", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "DEL", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = ".", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "*", onClick = { /*TODO*/ },modifier = Modifier)
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "(")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )  {
+                BotonCustom(text = "7", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "8", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "9", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "/", onClick = { /*TODO*/ },modifier = Modifier)
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = ")")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )  {
+                BotonCustom(text = "4", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "5", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "6", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "+", onClick = { /*TODO*/ },modifier = Modifier)
             }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "AC")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )  {
+                BotonCustom(text = "1", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "2", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "3", onClick = { /*TODO*/ },modifier = Modifier)
+                BotonCustom(text = "-", onClick = { /*TODO*/ },modifier = Modifier)
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "DEL")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = ".")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "*")
-            }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "7")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "8")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "9")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "/")
-            }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "4")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "5")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "6")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "+")
-            }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "1")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "2")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "3")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "-")
-            }
-        }
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "0")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "=")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            )  {
+                BotonCustom(
+                    text = "0",
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .padding(end = 4.dp)
+
+                )
+                BotonCustom(
+                    text = "=",
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .padding(start = 4.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun DisplayCalculadora(operacion: String,resultado: String){
-    Card (
+fun DisplayCalculadora(operacion: String, resultado: String) {
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(bottom = 20.dp)
     ) {
-        Column {
-            Text(text = operacion)
-            Text(text = resultado)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = operacion,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = androidx.compose.ui.text.style.TextAlign.End
+            )
+            Text(
+                text = resultado,
+                modifier = Modifier
+                    .fillMaxWidth(),
+
+                textAlign = androidx.compose.ui.text.style.TextAlign.End
+            )
         }
+    }
+}
+
+@Composable
+fun BotonCustom(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        ),
+        shape = RoundedCornerShape(12.dp),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 12.dp,
+            disabledElevation = 0.dp   
+        ),
+        modifier = modifier
+            .width(80.dp)
+            .height(80.dp)
+            .padding(bottom = 5.dp)
+    ) {
+        Text(text = text)
     }
 }
 
@@ -142,4 +208,10 @@ fun DisplayCalculadora(operacion: String,resultado: String){
 @Composable
 fun DisplayCalculadoraPreview(){
     DisplayCalculadora("operacion", "resultado")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculadoraPreview(){
+    Calculadora(modifier = Modifier)
 }
