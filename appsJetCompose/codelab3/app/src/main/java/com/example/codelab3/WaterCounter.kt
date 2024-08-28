50df3f719a8a08093505203acddff39f5a2ca50e
CarEsteban <car23016@uvg.edu.gt>
2024-08-28 17:08:17 -0600

    entender como funciona el state con los botones

--- appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WaterCounter.kt
+++ appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.codelab3
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,14 +20,27 @@ fun WaterCounter(modifier: Modifier = Modifier){
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
-        
         var count by remember { mutableIntStateOf(0) }
-        Text(text = "Has tomado $count vasos")
-        Button(
-            onClick = { count++ },
+        if (count>0){
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask){
+                WellnessTaskItem(taskName = "Ya tomaste 15 min de paseo?", onClose = { showTask = false})
+            }
+            Text(text = "Has tomado $count vasos")
+        }
+        Row (
             Modifier.padding(top = 8.dp)
-        ) {
-            Text(text = "Agrega uno")
+        ){
+            Button(
+                onClick = { count++ },
+                enabled = count<10
+            ) { Text(text = "Agrega uno") }
+            Button(
+                onClick = { count = 0},
+                Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Limpiar contador")
+            }
         }
     }
 }
