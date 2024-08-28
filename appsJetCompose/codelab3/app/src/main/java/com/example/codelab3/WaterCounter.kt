d10a5b33020730d326f6fbc6c94f71109b792d2c
CarEsteban <car23016@uvg.edu.gt>
2024-08-28 17:30:31 -0600

    listas de tareas

--- appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WaterCounter.kt
+++ appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.example.codelab3
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -11,36 +10,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        var count by remember { mutableIntStateOf(0) }
-        if (count>0){
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask){
-                WellnessTaskItem(taskName = "Ya tomaste 15 min de paseo?", onClose = { showTask = false})
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    StatelessCounter(count, {count++}, modifier)
+}
+
+@Composable
+fun StatelessCounter(
+        count: Int,
+        onIncrement: ()-> Unit,
+        modifier: Modifier = Modifier
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            if (count>0){
+                Text(text = "Has tomado $count vasos")
             }
-            Text(text = "Has tomado $count vasos")
-        }
-        Row (
-            Modifier.padding(top = 8.dp)
-        ){
             Button(
-                onClick = { count++ },
+                onClick = onIncrement,
+                Modifier.padding(top = 8.dp),
                 enabled = count<10
-            ) { Text(text = "Agrega uno") }
-            Button(
-                onClick = { count = 0},
-                Modifier.padding(start = 8.dp)
             ) {
-                Text(text = "Limpiar contador")
+                Text(text = "Agrega uno")
             }
         }
-    }
 }
--- appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WellnessTaskItem.kt
+++ appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WellnessTaskItem.kt
@@ -1,13 +1,20 @@
 package com.example.codelab3
 
+import android.widget.CheckBox
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,6 +22,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checkedThis: Boolean,
+    onCheckedChangeThis: (Boolean) -> Unit,
     onClose:()->Unit,
     modifier: Modifier = Modifier
 ){
@@ -28,9 +37,21 @@ fun WellnessTaskItem(
                 .padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(checked = checkedThis, onCheckedChange = onCheckedChangeThis)
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
 
     }
+}
+//me quede en el 10 Work With lists cuando definen esta funcion
+@Composable
+fun WellnessTaskItem(taskName: String,modifier: Modifier=Modifier){
+    var checkedState by remember { mutableStateOf(false) }
+    WellnessTaskItem(
+        taskName = taskName,
+        checkedThis = checkedState,
+        onCheckedChangeThis = {newValue -> checkedState = newValue},
+        onClose = { }
+    )
 }
