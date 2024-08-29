c64a6f522207074609a737d942c6fe6a0fd4506b
CarEsteban <car23016@uvg.edu.gt>
2024-08-28 20:49:24 -0600

    codelab3 terminado

--- appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WellnessTaskItem.kt
+++ appsJetCompose/codelab3/app/src/main/java/com/example/codelab3/WellnessTaskItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,36 +23,26 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
-    checkedThis: Boolean,
-    onCheckedChangeThis: (Boolean) -> Unit,
-    onClose:()->Unit,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
     modifier: Modifier = Modifier
-){
-    Row (
-        modifier = Modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ){
+) {
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
         Text(
             modifier = Modifier
                 .weight(1f)
                 .padding(start = 16.dp),
             text = taskName
         )
-        Checkbox(checked = checkedThis, onCheckedChange = onCheckedChangeThis)
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
-
     }
-}
-//me quede en el 10 Work With lists cuando definen esta funcion
-@Composable
-fun WellnessTaskItem(taskName: String,modifier: Modifier=Modifier){
-    var checkedState by remember { mutableStateOf(false) }
-    WellnessTaskItem(
-        taskName = taskName,
-        checkedThis = checkedState,
-        onCheckedChangeThis = {newValue -> checkedState = newValue},
-        onClose = { }
-    )
 }
