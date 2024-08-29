792311a2f7944bbcc55b5c38a36b62dd54575a9f
CarEsteban <car23016@uvg.edu.gt>
2024-08-29 12:49:12 -0600

    home screen creado

--- Lab6/app/src/main/java/com/example/lab6/Home.kt
+++ Lab6/app/src/main/java/com/example/lab6/Home.kt
@@ -1,2 +1,142 @@
 package com.example.lab6
 
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.StarOutline
+import androidx.compose.material.icons.filled.StarHalf
+
+
+@Composable
+fun Home(
+    modifier: Modifier = Modifier
+){
+
+    val robotoFontFamily = FontFamily(
+        Font(R.font.roboto_bold, FontWeight.Bold)
+    )
+
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+
+        ){
+            Icon(
+                painter = painterResource(id = R.drawable.btn_menu),
+                contentDescription = "btn_menu",
+                modifier = Modifier
+                    .size(24.dp),
+                tint = Color.Red
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 24.dp))
+            Text(
+                text = "POPULAR RECIPES",
+                modifier = Modifier,
+                fontFamily = robotoFontFamily,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red,
+                fontSize = 23.sp
+
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 24.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.btn_search),
+                contentDescription = "search",
+                modifier = Modifier
+                    .size(24.dp),
+                tint = Color.Red
+            )
+
+        }
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+            Spacer(modifier = Modifier.padding(vertical = 20.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "APPETIZERS")
+                Spacer(modifier = Modifier.padding(horizontal = 24.dp))
+                Text(text = "ENTREES")
+                Spacer(modifier = Modifier.padding(horizontal = 24.dp))
+                Text(text = "DESSERT")
+            }
+            Spacer(modifier = Modifier.padding(vertical = 12.dp))
+            LazyRow (
+                modifier = Modifier
+            ){
+                item {
+                    Box(
+                        modifier = Modifier
+                            .size(250.dp)
+                            .clip(RoundedCornerShape(20.dp))
+                    ){
+                        Image(
+                            painter = painterResource(id = R.drawable.costilla),
+                            contentDescription = "costilla",
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.FavoriteBorder,
+                            contentDescription = "favorite",
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .padding(16.dp)
+                                .size(24.dp),
+                            tint = Color.White
+                        )
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                        ){
+
+                        }
+                    }
+                }
+            }
+        }
+        Column {
+            Row {
+                
+            }
+            Text(text = "lorem")
+        }
+    }
+}
--- Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab6.ui.theme.Lab6Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize().padding(top = 35.dp) ) { innerPadding ->
+                    Home(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab6Theme {
-        Greeting("Android")
-    }
-}
