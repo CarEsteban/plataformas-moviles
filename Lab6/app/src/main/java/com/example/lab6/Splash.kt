3ca8c83086a4f0aeb6dff8e0ee980bf6f4e20249
Nicolás <con23197@uvg.edu.gt>
2024-08-29 12:12:37 -0600

    Imagen de splash con fondo rojo

--- Lab6/app/src/main/java/com/example/lab6/Splash.kt
+++ Lab6/app/src/main/java/com/example/lab6/Splash.kt
@@ -1,2 +1,35 @@
 package com.example.lab6
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Splash(modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .fillMaxSize()
+        .background(Color.Red).alpha(0.2f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pizza),
+            contentDescription = "Imagen de fondo",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSplash() {
+    Splash(modifier = Modifier.fillMaxSize())
+}
+
